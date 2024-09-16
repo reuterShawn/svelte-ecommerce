b601fe652bac6fe9ee725f88f9943edaa41e5bff
reuterShawn <reutershawn@proton.me>
2024-09-16 12:56:30 -0500

    update server

--- config/server.js
+++ config/server.js
@@ -8,3 +8,10 @@ module.exports = ({ env }) => ({
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:1337',
+});
+
+export default api;
