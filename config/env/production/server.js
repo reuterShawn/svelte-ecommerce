fc753e8d25706cc72dbfeb1f1ce0dff4d032f17a
reuterShawn <reutershawn@proton.me>
2024-09-24 15:21:23 -0500

    Edit server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,8 +1,5 @@
-// path: /config/env/production/server.ts
-export default ({ env }) => ({
-    proxy: true,
-    url: env('APP_URL'), // Sets the public URL of the application.
-    app: { 
-      keys: env.array('APP_KEYS')
-    },
+
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
 });
